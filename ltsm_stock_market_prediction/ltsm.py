2064d91459b06a0e22c032b78c6c17b492dc42af
Larry Johnson <johnson.larry.l@fintorian.com>
2023-09-12 20:20:42 -0500

    #13 - Wrapped model in OOP

--- ltsm_stock_market_prediction/ltsm.py
+++ ltsm_stock_market_prediction/ltsm.py
@@ -12,81 +12,97 @@ import yfinance as yf
 
 from ltsm_stock_market_prediction.conf import CONF
 
-# The tech stocks we'll use for this analysis
-TICKER_SYMBOLS = ['AAPL', 'GOOG', 'MSFT', 'AMZN']
+
+class LTSMModel:
+    # The tech stocks we'll use for this analysis
+    TICKER_SYMBOLS = ['AAPL', 'GOOG', 'MSFT', 'AMZN']
+
+    @staticmethod
+    def load_model(ticker_symbol: str, lags: int) -> Sequential:
+        model_pkl_file_name = CONF[ticker_symbol]["models"][str(lags)]
+
+        model_pkl_file = os.path.join(os.path.dirname(__file__),
+                                      f"model_repo/{model_pkl_file_name}")
+
+        model = load(model_pkl_file)
+
+        return model
+
+    @staticmethod
+    def compile_model(ticker_symbol: str,
+                      lags: int, start='2012-01-01',
+                      end=datetime.now(),
+                      epochs=1,
+                      batch_size=1) -> None:
+        # For reading stock data from yahoo
+        yf.pdr_override()
+
+        # Get the stock quote
+        df = pdr.get_data_yahoo(ticker_symbol, start=start, end=end)
+
+        # Create a new dataframe with only the 'Close column
+        data = df.filter(['Close'])
+        # Convert the dataframe to a numpy array
+        dataset = data.values
+
+        # Get the number of rows to train the model on
+        training_data_len = int(np.ceil(len(dataset) * .95))
+
+        scaler = MinMaxScaler(feature_range=(0, 1))
+        scaled_data = scaler.fit_transform(dataset)
+
+        # Create the training data set
+        # Create the scaled training data set
+        train_data = scaled_data[0:int(training_data_len), :]
+        # Split the data into x_train and y_train data sets
+        x_train = []
+        y_train = []
+
+        for i in range(60, len(train_data)):
+            x_train.append(train_data[i - 60:i, 0])
+            y_train.append(train_data[i, 0])
+
+        # Convert the x_train and y_train to numpy arrays
+        x_train, y_train = np.array(x_train), np.array(y_train)
+
+        # Reshape the data
+        x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+
+        # Build the LSTM model
+        model = Sequential()
+        model.add(LSTM(128, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+        model.add(LSTM(64, return_sequences=False))
+        model.add(Dense(25))
+        model.add(Dense(1))
+
+        # Compile the model
+        model.compile(optimizer='adam', loss='mean_squared_error')
+
+        # Train the model
+        model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs)
+
+        # Save model to disk
+        # save the LTSM RNN model as a pickle file
+        model_pkl_file_name = CONF[ticker_symbol]["models"][str(lags)]
+
+        model_pkl_file = os.path.join(os.path.dirname(__file__),
+                                      f"model_repo/{model_pkl_file_name}")
+
+        dump(model, model_pkl_file)
+
+    @staticmethod
+    def predict(ticker_symbol: str, lags: int, closing_prices: np.ndarray) -> float:
+        scaler = MinMaxScaler(feature_range=(0, 1))
+        expect_closing_prices_scaled = scaler.fit_transform(closing_prices)
+
+        model = LTSMModel.load_model(ticker_symbol=ticker_symbol, lags=lags)
+
+        prediction = model.predict(expect_closing_prices_scaled)
+        prediction = scaler.inverse_transform(prediction)
+
+        return prediction[-1:]
 
 
-def load_model(ticker_symbol: str, lags: int) -> Sequential:
-    model_pkl_file_name = CONF[ticker_symbol]["models"][str(lags)]
-
-    model_pkl_file = os.path.join(os.path.dirname(__file__),
-                                  f"model_repo/{model_pkl_file_name}")
-
-    model = load(model_pkl_file)
-
-    return model
-
-
-def compile_model(ticker_symbol: str,
-                  lags: int, start='2012-01-01',
-                  end=datetime.now(),
-                  epochs=1,
-                  batch_size=1) -> None:
-    # For reading stock data from yahoo
-    yf.pdr_override()
-
-    # Get the stock quote
-    df = pdr.get_data_yahoo(ticker_symbol, start=start, end=end)
-
-    # Create a new dataframe with only the 'Close column
-    data = df.filter(['Close'])
-    # Convert the dataframe to a numpy array
-    dataset = data.values
-
-    # Get the number of rows to train the model on
-    training_data_len = int(np.ceil(len(dataset) * .95))
-
-    scaler = MinMaxScaler(feature_range=(0, 1))
-    scaled_data = scaler.fit_transform(dataset)
-
-    # Create the training data set
-    # Create the scaled training data set
-    train_data = scaled_data[0:int(training_data_len), :]
-    # Split the data into x_train and y_train data sets
-    x_train = []
-    y_train = []
-
-    for i in range(60, len(train_data)):
-        x_train.append(train_data[i - 60:i, 0])
-        y_train.append(train_data[i, 0])
-
-    # Convert the x_train and y_train to numpy arrays
-    x_train, y_train = np.array(x_train), np.array(y_train)
-
-    # Reshape the data
-    x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
-
-    # Build the LSTM model
-    model = Sequential()
-    model.add(LSTM(128, return_sequences=True, input_shape=(x_train.shape[1], 1)))
-    model.add(LSTM(64, return_sequences=False))
-    model.add(Dense(25))
-    model.add(Dense(1))
-
-    # Compile the model
-    model.compile(optimizer='adam', loss='mean_squared_error')
-
-    # Train the model
-    model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs)
-
-    # Save model to disk
-    # save the LTSM RNN model as a pickle file
-    model_pkl_file_name = CONF[ticker_symbol]["models"][str(lags)]
-
-    model_pkl_file = os.path.join(os.path.dirname(__file__),
-                                  f"model_repo/{model_pkl_file_name}")
-
-    dump(model, model_pkl_file)
 
 
 
--- tests/test_setup_model.py
+++ tests/test_setup_model.py
@@ -1,22 +1,17 @@
-from ltsm_stock_market_prediction.ltsm import load_model
-from sklearn.preprocessing import MinMaxScaler
 from typing import List
+
+from ltsm_stock_market_prediction.ltsm import LTSMModel
 
 
 def test_appl_ltsm_60_day_model(appl_60_day_closing_prices: List[float]):
     assert (len(appl_60_day_closing_prices) > 0)
 
     expect_closing_price = 179.34976
+    ticker_symbol = "AAPL"
+    lags = 60
+    model = LTSMModel()
 
-    scaler = MinMaxScaler(feature_range=(0, 1))
-    expect_closing_prices_scaled = scaler.fit_transform(appl_60_day_closing_prices[0])
-
-    model = load_model(ticker_symbol="AAPL", lags=60)
-
-    prediction = model.predict(expect_closing_prices_scaled)
-    prediction = scaler.inverse_transform(prediction)
-
-    actual_closing_price = prediction[-1:]
+    actual_closing_price = model.predict(ticker_symbol, lags, appl_60_day_closing_prices[0])
 
     assert(expect_closing_price == actual_closing_price)
 
